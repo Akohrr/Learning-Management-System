ea3c8d043ef18237f3005c57f553f6213a6dd427
akohrr <atadoga.akoh@gmail.com>
2018-06-02 21:56:45 +0100

    edit settings file

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^vibz^7+fm5ee+fbvt)-fo-@0s)ehj_258y()gjx#j(s*5-izf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
