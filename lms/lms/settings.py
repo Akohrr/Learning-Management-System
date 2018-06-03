6d2edfff86acd97d795f92cab691950da99e1b95
akohrr <atadoga.akoh@gmail.com>
2018-06-03 03:36:37 +0100

    update settings.py

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -154,19 +154,19 @@ AUTHENTICATION_BACKENDS = (
     'guardian.backends.ObjectPermissionBackend',
 )
 
-in_heroku = False
+# in_heroku = False
 
-if 'DATABASE_URL' in os.environ:
-    in_heroku = True
+# if 'DATABASE_URL' in os.environ:
+#     in_heroku = True
 
 
-if in_heroku:
-    DATABASES = {'default': dj_database_url.config()}
+# if in_heroku:
+#     DATABASES = {'default': dj_database_url.config()}
 
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+# else:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 }
